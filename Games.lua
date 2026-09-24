a0f57c21ba49669843a04c47258356f933704d70
Mystery-Center <mysterycenter2000@gmail.com>
2026-09-23 22:57:17 -0600

    Update Muscle Legends URL in Games.lua

--- Games.lua
+++ Games.lua
@@ -1,5 +1,5 @@
 local Games = {
-	[1268927906] = "" -- Muscle Legends
+	[1268927906] = "https://raw.githubusercontent.com/Mystery-Center/Mystery-Loader/refs/heads/main/Roblox/Muscle%20Legends" -- Muscle Legends
 }
  
 return Games
